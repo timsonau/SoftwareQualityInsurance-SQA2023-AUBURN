529eff05bf2ae1558033e9a7833655c4108fde07
jacegrant <69464384+jacegrant@users.noreply.github.com>
2023-04-18 16:15:58 -0500

    update fuzz.py and runFuzz.yml

--- KubeSec/KubeSec-master/fuzz.py
+++ KubeSec/KubeSec-master/fuzz.py
@@ -10,19 +10,19 @@ def fuzzFunctions() :
     # all of these are just unexpected inputs that cause errors in different methods
 
     try : scanner.getYAMLFiles(12)
-    except Exception as e: ListOfExceptions.append(str(e))
+    except Exception as e: ListOfExceptions.append("Method Name: scanner.getYAMLFiles\nError: " + str(e))
 
     try : graphtaint.mineNetPolGraph(None, None, None, None)
-    except Exception as e: ListOfExceptions.append(str(e))
+    except Exception as e: ListOfExceptions.append("Method Name: graphtaint.mineNetPolGraph\nError: " + str(e))
 
     try : scanner.scanForUnconfinedSeccomp('')
-    except Exception as e: ListOfExceptions.append(str(e))
+    except Exception as e: ListOfExceptions.append("Method Name: scanner.scanForUnconfinedSeccomp\nError: " + str(e))
 
     try : main.getCountFromAnalysis('nothing')
-    except Exception as e: ListOfExceptions.append(str(e))
+    except Exception as e: ListOfExceptions.append("Method Name: main.getCountFromAnalysis\nError: " + str(e))
 
     try : parser.getValuesRecursively({})
-    except Exception as e: ListOfExceptions.append(str(e))
+    except Exception as e: ListOfExceptions.append("Method Name: parser.getValuesRecursively\nError: " + str(e))
 
     return ListOfExceptions
 
@@ -31,4 +31,4 @@ if __name__ == '__main__':
     results = fuzzFunctions()
 
     for i in results :
-        print('\nError: ' + i)
+        print('\n' + i)
