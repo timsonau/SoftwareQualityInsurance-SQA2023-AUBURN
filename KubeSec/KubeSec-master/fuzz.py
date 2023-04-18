c1021c8ee31d9a31d00b084432994e6a5e07f1f1
jacegrant <69464384+jacegrant@users.noreply.github.com>
2023-04-18 15:42:08 -0500

    test

--- KubeSec/KubeSec-master/fuzz.py
+++ KubeSec/KubeSec-master/fuzz.py
@@ -8,6 +8,7 @@ def fuzzFunctions() :
     ListOfExceptions = []
 
     # all of these are just unexpected inputs that cause errors in different methods
+    # test
 
     try : scanner.getYAMLFiles(12)
     except Exception as e: ListOfExceptions.append(str(e))
