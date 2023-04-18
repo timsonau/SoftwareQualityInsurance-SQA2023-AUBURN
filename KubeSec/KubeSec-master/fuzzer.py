d830927e720bdf6abbe92f354db8e62fd17ca4db
jacegrant <69464384+jacegrant@users.noreply.github.com>
2023-04-17 20:02:46 -0500

    fuzzer.py added

---
+++ KubeSec/KubeSec-master/fuzzer.py
@@ -0,0 +1,32 @@
+import scanner
+import graphtaint
+import parser
+import main
+
+def fuzzFunctions() :
+
+    ListOfExceptions = []
+
+    try : scanner.getYAMLFiles(12)
+    except Exception as e: ListOfExceptions.append(str(e))
+
+    try : graphtaint.mineNetPolGraph(None, None, None, None)
+    except Exception as e: ListOfExceptions.append(str(e))
+
+    try : scanner.scanForUnconfinedSeccomp('')
+    except Exception as e: ListOfExceptions.append(str(e))
+
+    try : main.getCountFromAnalysis('nothing')
+    except Exception as e: ListOfExceptions.append(str(e))
+
+    try : parser.getValuesRecursively({})
+    except Exception as e: ListOfExceptions.append(str(e))
+
+    return ListOfExceptions
+
+if __name__ == '__main__': 
+
+    results = fuzzFunctions()
+
+    for i in results :
+        print('\nError: ' + i)
