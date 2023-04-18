e318d90d59d8cd3c9708532da87d46fcae8ffb1d
jacegrant <69464384+jacegrant@users.noreply.github.com>
2023-04-18 15:25:50 -0500

    change name

--- KubeSec/KubeSec-master/fuzz.py
+++ KubeSec/KubeSec-master/fuzz.py
@@ -7,6 +7,8 @@ def fuzzFunctions() :
 
     ListOfExceptions = []
 
+    # all of these are just unexpected inputs that cause errors in different methods
+
     try : scanner.getYAMLFiles(12)
     except Exception as e: ListOfExceptions.append(str(e))
 
